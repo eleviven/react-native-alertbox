ee8f94b7af2d6eb5cb3909b613cfd04bd44dbc20
eleviven <firuz.haciyeff@gmail.com>
2021-03-02 21:40:14 +0400

    Example updated

--- examples/basic.js
+++ examples/basic.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {View, Button} from 'react-native';
-import {fire} from '../src/index';
+import {fire} from 'react-native-alertbox';
 
 export default function ExampleBasic() {
   return (
